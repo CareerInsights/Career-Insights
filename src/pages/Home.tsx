8f8b521d86ab1838facd224aea4581c97b0562d8
Luisgr <luijo915@hotmail.com>
2025-06-21 18:54:02 +0200

    initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,17 @@
 import "../styles/index.css"
 import { ButtonLink } from "../components/ButtonLink"
 import HeroBanner from "../features/components/HeroBanner"
+import GoalsSection from "../components/GoalsSection"
+import AboutSection from "../components/AboutSection"
 
 function Home() {
 
-
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-6 bg-gray-50 p-6">
-    <HeroBanner/>
 
+    <div className="min-h-screen flex flex-col items-center justify-center gap-6 bg-gray-50 ">
+      <HeroBanner/>
+      <GoalsSection />
+      <AboutSection />
       <h1 className="text-3xl font-bold underline text-gray-800">
         Test de orientación laboral
       </h1>
