9394b36c9c0612bebe09b70852e8b4648c2494b0
Adrian Lovera <105324331+loveradrian28@users.noreply.github.com>
2025-06-22 14:26:33 +0000

    Revert "Creado componente DianaSection con la tecnica diana y sus pasos, faltan las imagenes"
    This reverts commit 82135ae1b1079db7f6691891f754688681da803b.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import "../styles/index.css"
 import { ButtonLink } from "../components/ButtonLink"
 import HeroBanner from "../features/components/HeroBanner"
-import DianaSection from "../features/components/DianaSection"
 
 function Home() {
 
@@ -9,7 +8,7 @@ function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-6 bg-gray-50 p-6">
     <HeroBanner/>
-    <DianaSection/> 
+
       <h1 className="text-3xl font-bold underline text-gray-800">
         Test de orientación laboral
       </h1>
