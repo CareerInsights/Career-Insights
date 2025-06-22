c4485a7dc0e25e60d929e12b313237ba4bd54baf
Adrian Lovera <105324331+loveradrian28@users.noreply.github.com>
2025-06-22 17:02:39 +0000

    DianaSection terminada e implementada en su vista correspondiente

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -5,6 +5,7 @@ import Test from "../features/pages/Test";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Contact from "../pages/Contact";
+import DianaSection from "/workspaces/Career-Insights/src/pages/DianaSection.tsx";
 
 
 const AppRoutes = () => (
@@ -14,6 +15,7 @@ const AppRoutes = () => (
       <Route path="/" element={<Home />} />
       <Route path="/test" element={<Test />} />
       <Route path="/contacto" element={<Contact />} />
+      <Route path="/metodo" element={<DianaSection />} />
     </Routes>
     <Footer></Footer>
   </BrowserRouter>
